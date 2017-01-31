966ba6995f29b735db430d86ce59ab10c10ffb16
SaulSilver <hatem.houssein2gmail.com>
2017-01-31 22:18:57 +0100

    socket update

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ io.on('connection', function(socket) {
 });
 
 //Listening to webhooks
-app.post('/github', function (req, res) {
+app.post('/github/', function (req, res) {
     res.status(200);
     res.send();
     // console.log('request: \n' + req);
